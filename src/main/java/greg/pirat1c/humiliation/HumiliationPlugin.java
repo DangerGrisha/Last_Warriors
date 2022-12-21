8d914bcf1574be1a1167740254fadd3806bf1eb1
DangerGrisha <114128037+DangerGrisha@users.noreply.github.com>
2022-12-20 20:23:16 -0800

    added frog
    -added frog
    -changed local server path

--- src/main/java/greg/pirat1c/humiliation/HumiliationPlugin.java
+++ src/main/java/greg/pirat1c/humiliation/HumiliationPlugin.java
@@ -2,6 +2,7 @@ package greg.pirat1c.humiliation;
 
 import greg.pirat1c.humiliation.entity.HumiliationCommand;
 import greg.pirat1c.humiliation.entity.SpawnCommand;
+import greg.pirat1c.humiliation.entity.TestSpawn;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -12,7 +13,8 @@ public final class HumiliationPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
        getCommand(SPAWN_ENTITY_COMMAND_NAME).setExecutor(new SpawnCommand(this)); // Plugin startup logic
-       getCommand(HUMILIATION_COMMAND_NAME).setExecutor(new HumiliationCommand(this)); // Plugin startup logic
+       getCommand(HUMILIATION_COMMAND_NAME).setExecutor(new HumiliationCommand(this)); //
+        getCommand("test").setExecutor(new TestSpawn());// Plugin startup logic
 
     }
 
