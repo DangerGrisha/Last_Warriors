e37b08f4291b7c5684647157b7352f90664a4124
HR <gregory.romashchenko@gmail.com>
2024-04-22 16:27:46 -0700

    Almost done attraction + was add texture of 3dattraction to attraction

--- src/main/java/greg/pirat1c/humiliation/events/saske/AttractionListener.java
+++ src/main/java/greg/pirat1c/humiliation/events/saske/AttractionListener.java
@@ -2,10 +2,7 @@ package greg.pirat1c.humiliation.events.saske;
 
 import org.bukkit.*;
 import org.bukkit.block.Block;
-import org.bukkit.entity.ArmorStand;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -15,12 +12,17 @@ import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scoreboard.Team;
 import org.bukkit.util.Vector;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+
+import java.util.HashSet;
+import java.util.Set;
 
 
 public class AttractionListener implements Listener {
@@ -63,18 +65,30 @@ public class AttractionListener implements Listener {
         armorStand.setMarker(true); // Make armor stand a marker (invisible to players)
         armorStand.setCustomName("Attraction"); // Set custom name to identify the armor stand
         armorStand.setCustomNameVisible(false); // Make custom name invisible
+
+
+        // Add arms
+        armorStand.setArms(true);
+
+        // Give red dye to the right hand
+        ItemStack redDye = new ItemStack(Material.RED_DYE);
+        ItemMeta dyeMeta = redDye.getItemMeta();
+        dyeMeta.setDisplayName("Attraction");
+        redDye.setItemMeta(dyeMeta);
+        armorStand.getEquipment().setItemInMainHand(redDye);
+
     }
 
     private void startAttraction(Location location, Player placer) {
         new BukkitRunnable() {
             int ticks = 0;
             ArmorStand armorStand = null;
-            boolean damageDealt = false;
+            Set<LivingEntity> damagedEntities = new HashSet<>(); // Хранит противников, которые уже получили урон
 
             @Override
             public void run() {
                 ticks++;
-                if (ticks >= 100) { // 10 seconds
+                if (ticks >= 40) { // 10 seconds
                     if (armorStand != null && !armorStand.isDead()) {
                         armorStand.remove();
                     }
@@ -82,13 +96,17 @@ public class AttractionListener implements Listener {
                     return;
                 }
 
-                for (Entity entity : location.getWorld().getNearbyEntities(location, 10, 10, 10)) {
+                for (Entity entity : location.getWorld().getNearbyEntities(location, 7, 7, 7)) {
                     if (!(entity instanceof ArmorStand) && !(entity instanceof Player && isAlly((Player) entity, placer))) {
                         Vector direction = location.toVector().subtract(entity.getLocation().toVector()).normalize();
-                        entity.setVelocity(direction.multiply(0.1)); // Attract entities
-                        if (entity.getLocation().distanceSquared(location) <= 10 && entity instanceof LivingEntity && !damageDealt) { // Within 10 blocks
-                            ((LivingEntity) entity).damage(1); // Deal 1 damage
-                            damageDealt = true;
+                        entity.setVelocity(direction.multiply(0.5)); // Attract entities
+
+                        if (entity.getLocation().distanceSquared(location) <= 10 && entity instanceof LivingEntity) { // Within 10 blocks
+                            LivingEntity livingEntity = (LivingEntity) entity;
+                            if (!damagedEntities.contains(livingEntity)) {
+                                livingEntity.damage(5); // Deal 1 damage
+                                damagedEntities.add(livingEntity);
+                            }
                         }
                     }
                 }
@@ -99,6 +117,7 @@ public class AttractionListener implements Listener {
             }
         }.runTaskTimer(plugin, 20L, 2L); // Start after 1 second and repeat every 0.1 second
     }
+
 
 
 
@@ -141,6 +160,10 @@ public class AttractionListener implements Listener {
 
     private boolean isAlly(Player player, Player placer) {
         // Check if the player is in the same team as the placer
+        if (player.getGameMode() == GameMode.SPECTATOR) {
+            return true;
+        }
+
         Team placerTeam = placer.getScoreboard().getPlayerTeam(placer);
         Team playerTeam = player.getScoreboard().getPlayerTeam(player);
         return placerTeam != null && playerTeam != null && placerTeam.equals(playerTeam);
